859069d2b71affadada9c6dc5480e1cb03696ef9
Brix <bernardino.nb.bscs@gmail.com>
2022-06-30 17:58:57 +0800

    aa

---
+++ app/src/main/java/com/example/prototype10/depositmoney.java
@@ -0,0 +1,14 @@
+package com.example.prototype10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class depositmoney extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_depositmoney);
+    }
+}
